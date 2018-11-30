2b93be2429884bfd5f668641cf47ca2fb66d5e3a
Lucas Silveira <silveira.lsv@gmail.com>
2018-11-30 11:23:06 -0200

    Alterações classe teste

--- Mcc_Instaler/playground.py
+++ Mcc_Instaler/playground.py
@@ -4,21 +4,18 @@ import os
 import time
 import array as arr
 
-def shell(mensagem):
-    confirmar = os.system(mensagem)
-    if confirmar != 1:
-        return True
-    else:
-        return False
+
 
 def pre_install():
     teste_prerequisitos = InstalManager.teste_prerequisitos(InstalManager)
     if teste_prerequisitos:
-        funcionou = InstalManager.shell('sudo yum install python-pip')
-        if funcionou:
-            funcionou = InstalManager.shell('pip install --upgrade pip')
-
-
+        a =['sudo yum install python-pip', 'pip install --upgrade pip']
+        for x in range(0, a.__len__()):
+            funcionou = InstalManager.shell(a[x])
+            if funcionou:
+                x += 1
+            else:
+                break
 
 
 
