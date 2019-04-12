a7ade9323c9aaa0c81c7baaa8e92ea7cfacfe91a
Dois2 <silveira.lsv@gmail.com>
2019-04-12 10:48:14 -0300

    retirei o capitalize

--- Mcc_Instaler/Instalar.py
+++ Mcc_Instaler/Instalar.py
@@ -165,7 +165,7 @@ def escrever_arquivo_configjson(usuario):
         # print(Fore.RESET+Back.RESET)
         porta = '27017'
         try: 
-            nome_do_banco = get_user_mongo(usuario).capitalize
+            nome_do_banco = get_user_mongo(usuario)
         except IndexError:
             print('Nome de usuário no sigular,  banco referenciado somente como {}'.format(usuario))
             nome_do_banco = usuario.capitalize
